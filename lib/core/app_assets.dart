93bbed637bc7fcabc7c85a3b792ab71b8c72281c
AhmedSayeddd <ahmedsayedd1101@gmail.com>
2026-03-01 15:36:27 +0200

    fin_Core

--- lib/core/app_assets.dart
+++ lib/core/app_assets.dart
@@ -1,3 +1,28 @@
 class AppAssets {
-  
+  static const String logo = 'assets/images/logo.png';
+  static const String MoviesPosters = 'assets/images/MoviesPostersGroup.png';
+  static const String Acengers = 'assets/images/Avengers.png';
+  static const String OppenHeimer = 'assets/images/Oppenheimer.png';
+  static const String BadBoys = 'assets/images/BadBoys.png';
+  static const String DrString = 'assets/images/Drstrange.png';
+  static const String poster5 = 'assets/images/1917.png';
+  static const String forgotPassword = 'assets/images/Forgotpassword-bro.png';
+  static const String GamerProfile = 'assets/images/gamer1.png';
+  static const String GamerProfile1 = 'assets/images/gamer2.png';
+  static const String GamerProfile2 = 'assets/images/gamer3.png';
+  static const String GamerProfile3 = 'assets/images/gamer4.png';
+  static const String GamerProfile4 = 'assets/images/gamer5.png';
+  static const String GamerProfile5 = 'assets/images/gamer6.png';
+  static const String GamerProfile6 = 'assets/images/gamer7.png';
+  static const String GamerProfile7 = 'assets/images/gamer8.png';
+  static const String GamerProfile8 = 'assets/images/gamer9.png';
+  static const String email = 'assets/images/icons/email.svg';
+  static const String password = 'assets/images/icons/password.svg';
+  static const String phone = 'assets/images/icons/phone.svg';
+  static const String google = 'assets/images/icons/google.svg';
+  static const String egypt = 'assets/images/icons/EG.svg';
+  static const String usa = 'assets/images/icons/LR.svg';
+  static const String eyeOn = 'assets/images/icons/eyeOn.svg';
+  static const String eyeOff = 'assets/images/icons/eyeOff.svg';
+  static const String identity = 'assets/images/icons/Identification_.svg';
 }
--- lib/core/app_color.dart
+++ lib/core/app_color.dart
@@ -1,3 +1,8 @@
 class AppColor {
+  static const white = 0xFFFFFFFF;
+  static const black = 0xFF000000;
+  static const grey = 0xFF808080;
+  static const red = 0xFFFF0000;
+  static const gold = 0xFFf6bd00;
   
 }
